1aecfd0cb30602f34ffa5a63cb842bfdf1e99165
SHUBHSAXENA\shubh <shubhsaxena447@gmail.com>
2024-10-24 20:05:45 +0530

    added deployment configs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import connectDB from "./db/index.js";
-import {app} from "./app.js";
+import { app } from "./app.js";
 import dotenv from "dotenv";
 
 dotenv.config({
     path: './env'
-})
+});
 
-connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000,() => {
-        console.log(`⚙️   Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!! ", err);
-})
+let isConnected = false;
+
+export default async (req, res) => {
+    if (!isConnected) {
+        try {
+            await connectDB();
+            isConnected = true;
+            console.log("⚙️   MongoDB connected!");
+        } catch (err) {
+            console.error("MONGO db connection failed !!! ", err);
+            res.status(500).send("Database connection failed");
+            return;
+        }
+    }
+    
+    return app(req, res);
+};
