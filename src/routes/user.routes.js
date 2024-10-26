1ef09ac25733cb7695a93572791f540f6e7cd091
SHUBHSAXENA\shubh <shubhsaxena447@gmail.com>
2024-10-26 13:17:16 +0530

    Removed Cron jobs, added roll up api

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { getCitiesWeather,saveWeatherData } from "../controllers/user.controller.js";
+import { APIResponse } from "../utils/APIResponse.js";
 const router = Router()
 
 router.route("/weather").get((req,res) => {
@@ -7,4 +8,12 @@ router.route("/weather").get((req,res) => {
 });
 router.route("/get-weather-data").get(getCitiesWeather);
 router.route("/save-weather-data").post(saveWeatherData);
+router.route("/roll-up-weather-data").get(async (req,res) => {
+    console.log("Rolling up data");
+    const summary = await rollUpWeatherData()
+    if(!summary)
+        throw new Error("Something went wrong while data rolling up");
+    
+    res.status(201).json(new APIResponse(200,summary,"Rolled Up Successfully."))
+})
 export default router;
