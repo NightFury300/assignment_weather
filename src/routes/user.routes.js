89332c2dbd7020ab2d66c96f9af5f65744c36a69
SHUBHSAXENA\shubh <shubhsaxena447@gmail.com>
2024-10-26 13:31:18 +0530

    added key check

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { getCitiesWeather,saveWeatherData } from "../controllers/user.controller.js";
 import { APIResponse } from "../utils/APIResponse.js";
 import { rollUpWeatherData } from "../utils/weatherUtils.js";
+import { APIError } from "../utils/APIError.js";
 const router = Router()
 
 router.route("/weather").get((req,res) => {
@@ -10,6 +11,9 @@ router.route("/weather").get((req,res) => {
 router.route("/get-weather-data").get(getCitiesWeather);
 router.route("/save-weather-data").post(saveWeatherData);
 router.route("/roll-up-weather-data").get(async (req,res) => {
+    if(req.query.key !== "rollwd123")
+        res.status(400).json(new APIError(401,"Invalid Key"));
+    
     console.log("Rolling up data");
     const summary = await rollUpWeatherData()
     if(!summary)
