4cf042ba2f2ec01efb767ee579bed15680efdcda
SHUBHSAXENA\shubh <shubhsaxena447@gmail.com>
2025-01-03 15:27:31 +0530

    fixing

--- src/db/index.js
+++ src/db/index.js
@@ -4,6 +4,8 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
 try{
 const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+console.log(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
 console.log(`Connection Established Successfully At:${connectionInstance.connection.host}`);
 }
 catch(error){
