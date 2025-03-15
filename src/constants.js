f80e7cd8082983dcb42a09aa15d7aeb52b891d38
Shubh Saxena <shubhsaxena447@gmail.com>
2025-03-15 13:49:05 +0530

    changed db

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const DB_NAME = "expend";
+export const DB_NAME = "WeatherAPI";
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
