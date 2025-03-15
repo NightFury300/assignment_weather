1ddfbb18fcee177b56f7ca27587a179f204587f4
Shubh Saxena <shubhsaxena447@gmail.com>
2025-03-15 14:20:28 +0530

    path fixing yet again

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import cors from "cors"
 import path from "path" 
 import * as cron from "node-cron"
 import { rollUpWeatherData } from "./utils/weatherUtils.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const app = express();
 
@@ -14,8 +16,10 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 
 app.set("view engine","ejs");
-app.set("views", path.resolve(__dirname, "src", "views"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
+app.set("views", path.join(__dirname, "views"));
 const job = cron.schedule('55 23 * * *',async () => {
     console.log("Rolling up data");
     const summary = await rollUpWeatherData()
